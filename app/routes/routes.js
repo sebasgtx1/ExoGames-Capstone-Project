8743bcdb8cdf422afe68401388392ec152d37a75
SebastianBlandon <blandonodz@gmail.com>
2022-09-30 00:24:09 -0500

    [UPDATED] some files for deploy Landing page like a routes, index.webstatic, index.html, venues.html and package.json, [ADDED] images like events-default, user, venues, and files html competitors and users

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,12 +3,14 @@ const router = Router();
 
 
 const { getEvents, getEventId, createEvent, updateEvent, deleteEvent } = require('../controllers/events.controller');
-const { mainPage, loginPage, venuesPage } = require('../controllers/index.web_static');
+const { mainPage, loginPage, venuesPage, competitorsPage, usersPage } = require('../controllers/index.web_static');
 
 router.get('/', mainPage);
 router.get('/index.html', mainPage);
 router.get('/login', loginPage);
 router.get('/venues', venuesPage);
+router.get('/competitors', competitorsPage);
+router.get('/users', usersPage);
 router.get('/', getEvents);
 router.get('/event/:id', getEventId);
 router.post('/create_event', createEvent);

7503df3cb91d6e7f79218ceab70353955cdca938
SebastianBlandon <blandonodz@gmail.com>
2022-10-04 19:42:25 -0500

    [ADDED] fronend part with react routing, state and hooks

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
+import { get } from "../utils/httpClient";
 import { Card } from "./Card"
-import datas from "./datas.json"
 import styles from "./Grid.module.css"
 
 export function Grid() {
+    const [datas, setDatas] = useState([]);
+
+    useEffect(() => {
+        get("/discover/movie").then((data) => {
+        setDatas(data.results);
+        });
+    }, []);
     return (
         <ul className={styles.dataGrid}>
             {datas.map((data) => (

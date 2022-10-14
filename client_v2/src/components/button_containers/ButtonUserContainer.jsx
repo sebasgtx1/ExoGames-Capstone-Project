81b902960b62dadb1bd942ef5b32502fb129a48f
SebastianBlandon <blandonodz@gmail.com>
2022-10-13 23:45:20 -0500

    [UPDATED] functionality with dropdown for user login

--- client_v2/src/components/button_containers/ButtonUserContainer.jsx
+++ client_v2/src/components/button_containers/ButtonUserContainer.jsx
@@ -1,29 +1,45 @@
 import { Link } from 'react-router-dom'
 import stylesButton from "../styles/ButtonContainer.module.css"
-import stylesPullDown from "../styles/PullDown.module.css"
+import stylesDropdownUser from "../styles/DropdownUser.module.css"
 import { Button } from '../reusables/Button';
 import { ButtonNoStyle } from '../reusables/ButtonNoStyle';
 import { SearchBar } from '../reusables/SearchBar';
 import Swal from 'sweetalert2'
-import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle } from 'reactstrap'
 
 
 
 export function ButtonUserContainer(props) {
+    const [dropdown, setDropdown] = useState(false);
+
+    const openCloseDropdown = () => {
+        setDropdown(!dropdown);
+    }
     return (
         <div className={stylesButton.center_div}>
-            <aside className={`${stylesPullDown.dropdown_menu} ${stylesPullDown.dropdown_menu_sw}`}>
-                <div className={stylesPullDown.header_nav_current_user}><h1>Current User</h1></div>
-                <div className={stylesPullDown.dropdown_divider}></div>
-                <ButtonNoStyle src="/my_events" title="My Eventss"></ButtonNoStyle>
-                <ButtonNoStyle src="/my_venues" title="My Venues"></ButtonNoStyle>
-                <ButtonNoStyle src="/my_competitors" title=" My Competitors"></ButtonNoStyle>
-                <div className={stylesPullDown.dropdown_divider}></div>
-                <ButtonNoStyle src="/create_event" title="Create Event"></ButtonNoStyle>
-                <ButtonNoStyle src="/create_venue" title="Create Venue"></ButtonNoStyle>
-                <ButtonNoStyle src="/create_competitor" title="Create Competitor"></ButtonNoStyle>
-                <ButtonNoStyle src="/events"  title="Log out"></ButtonNoStyle>
+            <aside className={stylesDropdownUser.dropdown_menu}>
+                <Dropdown isOpen={dropdown} toggle={openCloseDropdown} size="lg">
+                    <DropdownToggle caret>
+                        Current User
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        <DropdownItem divider />
+                        <DropdownItem><Link to="/my_events" className={stylesDropdownUser.dataInsideButton}>My Events</Link>
+</DropdownItem>
+                        <DropdownItem><Link to="/my_venues" className={stylesDropdownUser.dataInsideButton}>My Venues</Link></DropdownItem>
+                        <DropdownItem><Link to="/my_competitors" className={stylesDropdownUser.dataInsideButton}>My Competitors</Link></DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem><Link to="/create_event" className={stylesDropdownUser.dataInsideButton}>Create Event</Link></DropdownItem>
+                        <DropdownItem><Link to="/create_venue" className={stylesDropdownUser.dataInsideButton}>Create Venue</Link></DropdownItem>
+                        <DropdownItem><Link to="/create_competitor" className={stylesDropdownUser.dataInsideButton}>Create Competitor</Link></DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem><Link to="/events" className={stylesDropdownUser.dataInsideButton}>Log Out</Link></DropdownItem>
+                        <DropdownItem divider />
+                    </DropdownMenu>
+                </Dropdown>
             </aside>
             <ul className={stylesButton.dataButtons}>
                 <Button src={"/events/" + props.user_id} title="Events"></Button>

4de2e2e02c9d8f4a77d1b7a388f1cd6d0aa34ba5
Dontcode44 <kabutto378@gmail.com>
2022-10-15 19:02:00 -0500

    Add - Controllers users and routes ♿

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -44,4 +44,50 @@ module.exports = {
       });
     });
   },
+  getUsers: (req, res) => {
+    getUsers((err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
+  updateUsers: (req, res) => {
+    const body = req.body;
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    updateUser(body, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return res.json({
+        success: 1,
+        message: "updated successfully",
+      });
+    });
+  },
+  deleteUser: (req, res) => {
+    const data = req.body;
+    deleteUser(data, (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      if (!results) {
+        return res.json({
+          success: 0,
+          message: "Record Not Found",
+        });
+      }
+      return res.json({
+        success: 1,
+        message: "user deleted successfully",
+      });
+    });
+  },
 };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,16 @@
-const { createUser } = require("../controllers/users.controller");
+const {
+  createUser,
+  getUserById,
+  getUsers,
+  updateUsers,
+  deleteUser,
+} = require("../controllers/users.controller");
 const router = require("express").Router();
 
 router.post("/create_user", createUser);
+router.get("/getUsers", getUsers);
+router.get("/get_user/:id", getUserById);
+router.patch("/update_user", updateUsers);
+router.delete("/delete_user:", deleteUser);
 
 module.exports = router;

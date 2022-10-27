e00d64495053c3c6168cf3bb51d6fe62f2cc64fd
Dontcode44 <kabutto378@gmail.com>
2022-10-27 10:35:15 -0500

    Add - Db with timestamps

--- server/controllers/db_conexion.js
+++ server/controllers/db_conexion.js
@@ -4,6 +4,9 @@ const sequelize = new Sequelize("defaultdb", "doadmin", "AVNS_Iux7HlByaV2ih-VMia
   host: "db-mysql-nyc1-61838-do-user-12661680-0.b.db.ondigitalocean.com",
   port: "25060",
   dialect: "mysql",
+  define: {
+    timestamps: false,
+  },
 });
 
 module.exports = {
